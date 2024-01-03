0de1f278813271258a63f30a8164c185d60e0e41
Nick French <nickfrench@Nicks-MacBook-Pro.local>
2024-01-02 21:34:39 -0600

    floatsum

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-"""sumfloat"""
+"""returnfloatsum"""
 
 
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    total_sum: float = 0.0
-    for num in input_list:
-        total_sum += num
-        """floatsum"""
-    return total_sum
+    """floatsum"""
+    return sum(input_list)
