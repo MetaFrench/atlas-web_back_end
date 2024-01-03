3ee7e042915a7e9b31d02dfc02be2122b35242c5
Nick French <nickfrench@Nicks-MacBook-Pro.local>
2024-01-03 10:05:20 -0600

    union

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
-"""mixlist"""
+"""union"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """returns sum of integers and floats in list"""
+    return sum(mxd_lst)
