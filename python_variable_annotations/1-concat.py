de0833ab3e5616a5158841dd4e2014bfaf9a805b
Nick French <nickfrench@Nicks-MacBook-Pro.local>
2024-01-02 18:34:39 -0600

    string

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """concatstring"""
 
-str1 = "egg"
-str2 = "shell"
-"""initialstrings"""
-str3 = str1 + str2
-print(str3)
+
+def concat(str1: str, str2: str) -> str:
+    """returnstring"""
+    return str1 + str2
