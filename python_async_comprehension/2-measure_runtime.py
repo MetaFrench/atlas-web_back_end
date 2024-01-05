2f753cb9264206bd9ea339bb2805b713bd3bde1c
Nick French <nickfrench@Nicks-MacBook-Pro.local>
2024-01-05 16:12:17 -0600

    time

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+import asyncio
+from typing import List
+from time import perf_counter
+
+
+async def measure_runtime() -> float:
+    start_time = perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = perf_counter()
+    return end_time - start_time
