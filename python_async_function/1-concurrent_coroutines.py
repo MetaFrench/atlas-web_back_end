cc8d217706454dbc61a32b452e8fe97e31e76729
Nick French <nickfrench@Nicks-MacBook-Pro.local>
2024-01-05 15:06:14 -0600

    concurrent

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
-"""async"""
-
+"""hello"""
 
 import asyncio
+import random
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """hello"""
+    async def wait_random(max_delay: int) -> float:
+        return random.uniform(0, max_delay)
+
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return await asyncio.gather(*delays)
