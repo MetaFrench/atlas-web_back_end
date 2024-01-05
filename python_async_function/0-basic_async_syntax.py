57eedf192680b6a98fddf635a7338c681c34fd38
Nick French <nickfrench@Nicks-MacBook-Pro.local>
2024-01-05 13:54:03 -0600

    basic

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-
 """delay"""
+
+
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-     """maxdelay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
+    """maxdelay"""
     return delay
