64740a09b4733c2022cdf55b793a6ad9367ad336
Nick French <nickfrench@Nicks-MacBook-Pro.local>
2024-01-09 14:33:32 -0600

    lifo

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-"""Last-In-First-Out (LIFO) Caching"""
+"""Lifo caching"""
 
 from BaseCaching import BaseCaching
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """Class implementing LIFO caching strategy"""
+    """created class LIFO"""
 
     def __init__(self):
-        """Initializes the LIFO cache"""
+        """Calling parent"""
         super().__init__()
-        self.queue = []  # List to maintain the order of keys
+        self.queue = []
 
     def put(self, key, item):
-        """Adds or updates an item in the cache"""
+        """Set the key"""
         if key is None or item is None:
             return
 
@@ -25,18 +25,18 @@ class LIFOCache(BaseCaching):
             self.queue.remove(key)
         else:
             if len(self.cache_data) >= self.MAX_ITEMS:
-                """LIFO: Remove the last item added to the cache"""
+                """ LIFO: Remove the last item added to the cache"""
                 last_key = list(self.cache_data.keys())[-1]
                 del self.cache_data[last_key]
                 print(f"DISCARD: {last_key}")
 
-        """Add or update the item in the cache"""
+        """ Add or update the item in the cache """
         self.cache_data[key] = item
         """Append the new key to the end of the queue"""
         self.queue.append(key)
 
     def get(self, key):
-        """Retrieves the value associated with the given key"""
+        """ return the cache"""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
