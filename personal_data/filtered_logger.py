2c17e84d466935af453559fbbd1b4b5af43cb88f
Nick French <nickfrench@Nicks-MacBook-Pro.local>
2024-01-15 13:32:14 -0600

    filter

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for log filtering
+"""
+
+import re
+
+
+def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+    """
+    Obfuscate specified fields in the log message.
+    Returns:
+    - String: Log message with specified fields obfuscated.
+    """
+    return re.sub(fr'(?<={separator}|^)({"|".join(fields)})=[^{separator}]+', f'\\1={redaction}', message)
