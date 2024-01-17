e24ae456aaa99accf5d3c2661ff45a7a7ab1e3c0
Nick French <nickfrench@Nicks-MacBook-Pro.local>
2024-01-16 22:07:57 -0600

    encrypt

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 """
-Contains a `hash_password` function that returns a salted,
-hashed password, which is a byte string.
+Contains functions for hashing passwords and validating them.
 """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """
-    Function that takes in a password string and returns
-    a salted, hashed password, which is a byte string.
+    Hashes the provided password with a generated salt
+    and returns the resulting byte string.
     """
     encoded_password = password.encode('utf-8')
-    return (bcrypt.hashpw(encoded_password, bcrypt.gensalt()))
+    return bcrypt.hashpw(encoded_password, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates whether the provided password matches the
+    previously hashed password.
+    """
+    encoded_password = password.encode('utf-8')
+    return bcrypt.checkpw(encoded_password, hashed_password)
