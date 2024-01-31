d2d61a6233d1767e5cd3c79722abd127fddab04e
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-01-31 17:13:27 -0600

    client

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Test Suite for testing client.py"""
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 
 
@@ -19,6 +19,19 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             f'https://api.github.com/orgs/{org_name}')
         self.assertEqual(org_info, {"name": "test_org"})
+
+    @patch('client.GithubOrgClient.org', return_value={"repos_url": "https://api.github.com/orgs/test_org/repos"})
+    def test_public_repos_url(self, mock_org):
+        github_client = GithubOrgClient("test_org")
+
+        # Mock the memoized property
+        with patch.object(GithubOrgClient, '_public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = "https://api.github.com/orgs/test_org/repos"
+
+            # Call the method and assert the result
+            result = github_client._public_repos_url
+            self.assertEqual(
+                result, "https://api.github.com/orgs/test_org/repos")
 
 
 if __name__ == "__main__":
