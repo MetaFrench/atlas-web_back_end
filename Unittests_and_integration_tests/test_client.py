ffaa53e4735681ce4e6f45990f69150e6f0bbc20
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-01-31 18:10:01 -0600

    client

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -8,16 +8,12 @@ from client import GithubOrgClient
 
 class TestGithubOrgClient(unittest.TestCase):
 
-    @parameterized.expand([
-        ("google",),
-        ("abc",),
-    ])
     @patch('client.get_json', return_value={"name": "test_org"})
-    def test_org(self, org_name, mock_get_json):
-        github_client = GithubOrgClient(org_name)
+    def test_org(self, mock_get_json):
+        github_client = GithubOrgClient("test_org")
         org_info = github_client.org()
         mock_get_json.assert_called_once_with(
-            f'https://api.github.com/orgs/{org_name}')
+            f'https://api.github.com/orgs/test_org')
         self.assertEqual(org_info, {"name": "test_org"})
 
     @patch('client.GithubOrgClient.org', return_value={"repos_url": "https://api.github.com/orgs/test_org/repos"})
@@ -32,6 +28,23 @@ class TestGithubOrgClient(unittest.TestCase):
             result = github_client._public_repos_url
             self.assertEqual(
                 result, "https://api.github.com/orgs/test_org/repos")
+
+    @patch('client.GithubOrgClient._public_repos_url', return_value="https://api.github.com/orgs/test_org/repos")
+    @patch('client.get_json', return_value=[{"name": "repo1"}, {"name": "repo2"}])
+    def test_public_repos(self, mock_get_json, mock_public_repos_url):
+        github_client = GithubOrgClient("test_org")
+
+        # Call the method and assert the result
+        repos = github_client.public_repos()
+        expected_repos = [{"name": "repo1"}, {"name": "repo2"}]
+
+        # Assert that the mocks were called once
+        mock_get_json.assert_called_once_with(
+            "https://api.github.com/orgs/test_org/repos")
+        mock_public_repos_url.assert_called_once()
+
+        # Assert the result
+        self.assertEqual(repos, expected_repos)
 
 
 if __name__ == "__main__":
