2db27453bd879c0dfa1ce03272e8f4c356e0c7e3
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-02-08 13:51:52 -0600

    bytes

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
-
 import redis
 import uuid
 
+
 class Cache:
-    def __init__(self) -> None:
+
+    def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn=None):
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key):
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key):
+        return self.get(key, int)
