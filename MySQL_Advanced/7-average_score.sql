634d217ae76161696ea48a652d4767cc9eb72c07
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-02-14 18:07:21 -0600

    score

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,6 +1,7 @@
 -- SQL script that creates a stored procedure
 -- `ComputeAverageScoreForUser` that computes and stores
 -- the average score for a student.
+DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
@@ -8,3 +9,6 @@ BEGIN
     SELECT AVG(score) INTO avg_score FROM corrections WHERE corrections.user_id = user_id;
     UPDATE users SET average_score = avg_score WHERE id = user_id;
 END;
+//
+
+DELIMITER ;
