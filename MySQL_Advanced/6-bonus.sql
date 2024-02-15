8838bb4f849e7702ed9ec00ecf15b7a6ca86d14c
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-02-14 18:00:08 -0600

    bonus

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,16 +1,16 @@
 -- SQL script that creates a stored procedure `AddBonus`
 -- that adds a new correction for a student.
-CREATE PROCEDURE AddBonus (
-    IN user_id INT, 
-    IN project_name VARCHAR(255),
-    IN score INT
-)
-BEGIN
-  INSERT INTO projects (name) 
-  SELECT project_name 
-  WHERE NOT EXISTS (SELECT 1 FROM projects WHERE name = project_name);
-  
-  INSERT INTO corrections (user_id, project_id, score)
-  VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+DELIMITER //
 
-END;
+CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    DECLARE project_id INT;
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END;//
+
+DELIMITER ;
